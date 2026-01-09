7870c2a4dc2530d98916152de34fa0df8d55fb6c
EwanKambang <ewankambang@proton.me>
2026-01-09 22:42:02 +0000

    colorchaneg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,42 @@
+-- return {
+--   {
+--     "ellisonleao/gruvbox.nvim",
+--     opts = {
+--       transparent_mode = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--     },
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+--
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
     opts = {
-      transparent_mode = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      transparent = true,
+      dimInactive = false,
+      -- You can set a default variant if you like:
+      theme = "zen", -- or "zen", "mist", "pearl"
+      -- background = { dark = "ink", light = "pearl" },
     },
+    config = function(_, opts)
+      require("kanso").setup(opts)
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "kanso",
     },
   },
 }
--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -2,6 +2,16 @@ return {
   "benlubas/molten-nvim",
   version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
   build = ":UpdateRemotePlugins",
+  -- useful keymaps for common molten actions
+  keys = {
+    { "<leader>ml", "<cmd>MoltenEvaluateLine<cr>", desc = "Molten: evaluate line" },
+    { "<leader>mv", "<cmd>MoltenEvaluateVisual<cr>", mode = "v", desc = "Molten: evaluate selection" },
+    { "<leader>mo", "<cmd>MoltenEnterOutput<cr>", desc = "Molten: open output window" },
+    { "<leader>mi", "<cmd>MoltenInit<cr>", desc = "Molten: init kernel" },
+    { "<leader>mI", "<cmd>MoltenDeinit<cr>", desc = "Molten: deinit kernel" },
+    { "<leader>mk", "<cmd>MoltenInterrupt<cr>", desc = "Molten: interrupt kernel" },
+    { "<leader>mR", "<cmd>MoltenRestart<cr>", desc = "Molten: restart kernel" },
+  },
   init = function()
     -- this is an example, not a default. Please see the readme for more configuration options
     vim.g.molten_output_win_max_height = 12
